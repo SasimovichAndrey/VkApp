48274119cf50083280cc322933b48785a134f668
Sasimovich Andrey <ballack.tyta@gmail.com>
2018-06-22 17:39:35 +0300

    refactoring prev commit

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,1 +1,33 @@
-var token = localStorage.getItem('vk_access_token');
+var accessToken = localStorage.getItem('vk_access_token');
+var userId = localStorage.getItem('vk_user_id');
+
+if(accessToken != null){
+    getUserFriends(getUserFriendSuccess);
+
+    function getUserFriendSuccess(){
+        
+    }
+
+    function saveUserFriendsToDb(userId, friendIds){
+        var reqBody = {
+            userId: userId,
+            friendIds: friendIds
+        };
+        var reqBodyJson = JSON.stringify(reqBody);
+
+        var backendRequest = new XMLHttpRequest();
+        backendRequest.open('POST', `${BACKEND_API_URL}/addUserFriends`);
+        backendRequest.setRequestHeader('Content-Type', 'application/json');
+        backendRequest.send(reqBodyJson);
+    }
+    
+    function getUserFriends(callback){
+        var callbackFuncName = 'getUserFriendsCallback';
+        window[callbackFuncName]= (result) => {
+            callback(userId, result.response.items);
+        }
+        var script = document.createElement('SCRIPT'); 
+        script.src = `https://api.vk.com/method/friends.get?&v=${VK_API_VERSION}&callback=${callbackFuncName}&access_token=${accessToken}`; 
+        document.getElementsByTagName("head")[0].appendChild(script); 
+    }
+}
