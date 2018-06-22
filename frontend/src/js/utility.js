62858d77dc46f055441b98e6db01919144ca51bd
AndreiSasimovich <ballack.tyta@gmail.com>
2018-06-22 13:43:27 +0300

    added storing acces token to localStorage

---
+++ frontend/src/js/utility.js
@@ -0,0 +1,12 @@
+function findLocationHashParameter(parameterName){
+    var result = null,
+        tmp = [];
+    location.hash
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+            tmp = item.split("=");
+            if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
+}
